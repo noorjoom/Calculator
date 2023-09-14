138e81b8f8ab86abd7f55c741fb3c5c8c85cf55e
Noor Joomratty <noor@joomratty.com>
2023-09-14 18:41:42 +0200

    Change logic in JS --a lot of logic

--- script.js
+++ script.js
@@ -1,104 +1,85 @@
-function add(a, b) {
-    return a + b;
-}
-
-function substract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return console.error("not allowed to divide against 0!");
-    }
-}
-
-let n1 = 0;
-let n2 = null;
-let operator = null;
-
-function operate(n1, n2, operator) {
-    if (operator === "+") {
-        return add(n1, n2);
-    } else if (operator === "-") {
-        return substract(n1, n2);
-    } else if (operator === "*") {
-        return multiply(n1, n2);
-    } else if (operator === "/") {
-        return divide(n1,n2);
-    } else {
-        return "Something is wrong here";
-    }
-}
 let currentInput = "";
+let currentNumber = null;
+let operator = null;
 const display = document.getElementById('result');
 
-function populateDisplay() {
-
-    const numberButtons = document.querySelectorAll('.number');
-
-    numberButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            const clickedNumber = event.target.textContent;
-
-            currentInput += clickedNumber;
-            display.value = currentInput;
-        });
-    });
-
-
-    const clearButton = document.querySelector('.clear');
-    clearButton.addEventListener("click", (event) => {
-        display.value = 0;
-        currentInput = "";
-        n1 = 0;
-        n2 = null;
-        operator = null;
-    });
-
-    //Not working!
-    const operatorButtons = document.querySelectorAll('.operator');
-    operatorButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            if (n1 !== null && currentInput !== "") {
-                //check for chain operations (n2 !== null)
-                if (n2 !== null) {
-                    n1 = operate(n1, parseFloat(currentInput), operator);
-                    n2 = null;
-                    operator = null;
-                    currentInput = "";
-                    display.value = n1;
-                } else {
-                    n2 = parseFloat(currentInput);
-                    currentInput ="";
-                }
-            }
-
-            operator = event.target.textContent;
-            // if (n1 === 0) {
-            //     n1 = parseFloat(currentInput);
-            //     currentInput = "";
-            // }
-        });
-    });
-
-    const equalButton = document.querySelector('.equal');
-    equalButton.addEventListener("click", (event) => {
-        if (operator && currentInput) {
-            n2 = parseFloat(currentInput);
-            const result = operate(n1, n2, operator);
-            display.value = result;
-            n1 = result;
-            n2 = null;
-            operator = null;
-            currentInput = "";
-        }
-    });
+function clearDisplay() {
+    display.value = '0';
 }
 
-populateDisplay();
+function updateDisplay() {
+    if (currentNumber !== null) {
+        display.value = currentNumber;
+    } else {
+        display.value = currentInput;
+    }
+}
+
+function handleNumberClick(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function handleOperatorClick(newOperator) {
+    if (currentNumber === null) {
+        currentNumber = parseFloat(currentInput);
+        currentInput = "";
+    } else if (currentInput !== "") {
+        currentNumber = operate(currentNumber, parseFloat(currentInput), operator);
+        currentInput = "";
+    }
+    operator = newOperator;
+    updateDisplay();
+}
+
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            if (b !== 0) {
+                return a / b;
+            } else {
+                console.error("Not allowed to divide by 0!");
+                return null;
+            }
+        default:
+            return null;
+    }
+}
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        handleNumberClick(event.target.textContent);
+    });
+});
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener("click", () => {
+    currentInput = "";
+    currentNumber = null;
+    operator = null;
+    clearDisplay();
+});
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        handleOperatorClick(event.target.textContent);
+    });
+});
+
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener("click", () => {
+    if (currentNumber !== null && operator !== null && currentInput !== "") {
+        currentNumber = operate(currentNumber, parseFloat(currentInput), operator);
+        operator = null;
+        currentInput = "";
+        updateDisplay();
+    }
+});
